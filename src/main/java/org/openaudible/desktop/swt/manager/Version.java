037a75836be499cb0482c2c52a15b0541e443e4e
openaudible <racer@torguard.tg>
2018-09-09 20:40:19 -0700

    vers

--- src/main/java/org/openaudible/desktop/swt/manager/Version.java
+++ src/main/java/org/openaudible/desktop/swt/manager/Version.java
@@ -3,7 +3,7 @@ package org.openaudible.desktop.swt.manager;
 public interface Version {
 	
 	String appName = "OpenAudible";
-	String appVersion = "1.1.7";
+	String appVersion = "1.1.8";
 	boolean appDebug = false;
 	String appLink = "http://openaudible.org";
 	String versionLink = "http://openaudible.org/swt_version.json";
